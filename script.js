3eed1882119cf2784867c8191956d10945c8a7d5
TONG EN SHIH <413570024@m365.fju.edu.tw>
2025-10-21 13:47:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $("#btnShow").click(function() {
-    $("#msg").hide().html("🎉 恭喜你！jQuery 成功運作啦！🎉").fadeIn(1000);
+    $("#msg").hide().html("🎉 嗨！🎉").fadeIn(1000);
   });
 });

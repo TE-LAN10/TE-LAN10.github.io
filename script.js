92ebdf65253f76be6253ffd562419c384c3cd72b
TONG EN SHIH <413570024@m365.fju.edu.tw>
2025-10-21 13:50:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
+
+  // 🌙 切換黑/白主題
+  $("#themeToggle").click(function() {
+    $("body").toggleClass("dark");
+
+    if ($("body").hasClass("dark")) {
+      $("#themeToggle").text("切換成亮色模式");
+    } else {
+      $("#themeToggle").text("切換成暗色模式");
+    }
+  });
+
+  // 🎉 顯示訊息
   $("#btnShow").click(function() {
-    $("#msg").hide().html("🎉 嗨！🎉").fadeIn(1000);
+    $("#msg")
+      .hide()
+      .html("🎉 嗨！🎉")
+      .fadeIn(800);
   });
 });
